df6241d55365d7c55b0b7a1f1e2349b64b77270c
Elliott Claus <e.pneve.seven@gmail.com>
2023-04-16 23:16:55 -0400

    new keymap done, lights not done

---
+++ keyboards/crkbd/keymaps/emdashii36/keycodes.h
@@ -0,0 +1,69 @@
+/* Copyright 2022 toinux
+  * Copyright 2023 emdashii
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+#pragma once
+
+enum crkbd_layers {
+  _QWERTY,
+  _NAVIGATION,
+  _MINECRAFT,
+  _NUMBER,
+  _FUNCTION,
+  _ADJUST
+};
+
+
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
+// Special layer navigation keys
+#define SQWERTY DF(_QWERTY) // sets qwerty as base layer
+#define LQWERTY TO(_QWERTY)
+#define LNAV    TO(_NAVIGATION)
+#define LADJUST TO(_ADJUST)
+#define LNUM    TO(_NUMBER)
+#define LFUN    TO(_FUNCTION)
+#define LMINECR TO(_MINECRAFT)
+
+// Misc combined keys
+#define CTRLSPC LCTL_T(KC_SPC)
+#define CTRLSFT C(KC_LSFT)
+#define COLON S(KC_SCLN)
+#define TABLGUI LGUI_T(KC_TAB)
+#define LCAPSFT LSFT_T(KC_CAPS)
+
+// NEW for this keymap
+#define MEH_SPC MEH_T(KC_SPC)
+#define SFT_SFT LSFT_T(OSM(KC_LSFT)) // hold for shift, or tap to shift one key
+#define NUM_G   LT(_NUMBER, KC_G)
+#define NUM_H   LT(_NUMBER, KC_H)
+#define FUN_B   LT(_FUNCTION, KC_B)
+#define FUN_N   LT(_FUNCTION, KC_N)
+
+// Tab movement
+#define TABFWRD C(KC_TAB)
+#define TABBKWD C(S(KC_TAB))
+
+//OSM(MOD_LSFT) One Shot Modifer Shift Key
---
+++ keyboards/crkbd/keymaps/emdashii36/rgb.c
@@ -0,0 +1,157 @@
+/* Copyright 2022 @toinux
+  * Copyright 2023 @emdashii
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+
+#include QMK_KEYBOARD_H
+#include "keycodes.h"
+
+static const char qwerty_leds[] = {22, 19, 16, 11, 38, 43, 46, 49};
+static const char gaming_leds[] = {18, 22, 19, 16, 13};
+static const char sym_leds[] = {22,19,16,11,21,20,15,12,34,35,38,39,43,23,25,7,49,48};
+static const char nav_leds[] = {38, 43, 44, 46};
+static const char num_leds[] = {45, 44, 37, 46, 43, 38, 47, 42, 39, 40};
+static const char fun_leds[] = {45, 44, 37, 46, 43, 38, 47, 42, 39, 50, 49, 48};
+static const char mouse_leds[] = {11, 16, 17, 19};
+static const char adjust_leds[] = {25, 22, 19, 16, 26, 21, 20, 15};
+static const char top_right_leds[] = {23, 18, 17, 10, 9};
+static const char adjust_other_leds[] = {36, 37, 44, 50, 51, 49, 9};
+
+
+/*
+  _QWERTY = RGB_CORAL   (calm white)
+  _NAVIGATION = RGB_PINK
+  _GAMING = RGB_SPRINGGREEN
+  _NUMBER = RGB_GOLDENROD
+  _SYMBOL = RGB_ORANGE
+  _FUNCTION = RGB_MAGENTA
+  _ADJUST = RGB_RED
+*/
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(13, RGB_RED);
+    }
+    switch(get_highest_layer(layer_state|default_layer_state)) {
+        case _QWERTY:
+            for (uint8_t i = 0; i < 8; i++) {
+                rgb_matrix_set_color(qwerty_leds[i], RGB_CORAL);
+            }
+            rgb_matrix_set_color(13, RGB_GOLDENROD);        // To Numbers
+            rgb_matrix_set_color(6, RGB_ORANGE);            // To Symbols
+            rgb_matrix_set_color(40, RGB_PINK);             // To Nav
+            rgb_matrix_set_color(6, RGB_MAGENTA);          // To Function keys
+            break;
+        case _NAVIGATION:
+            for (uint8_t i = 0; i < 4; i++) {
+                rgb_matrix_set_color(nav_leds[i], RGB_PINK);
+                rgb_matrix_set_color(mouse_leds[i], RGB_PINK);
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            rgb_matrix_set_color(41, RGB_RED);              // To Adjust
+            rgb_matrix_set_color(15, RGB_MAGENTA);          // To Function keys
+            break;
+        case _GAMING:
+            if (is_keyboard_master()) {
+                for (uint8_t i = 0; i < 5; i++) {
+                    rgb_matrix_set_color(gaming_leds[i], RGB_SPRINGGREEN);
+                }
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            rgb_matrix_set_color(13, RGB_SPRINGGREEN);      // Spacebar
+            rgb_matrix_set_color(40, RGB_MAGENTA);          // To function keys
+            rgb_matrix_set_color(14, RGB_MAGENTA);          // To function keys
+            break;
+        case _NUMBER:
+            for (uint8_t i = 0; i < 10; i++) {
+                rgb_matrix_set_color(num_leds[i], RGB_GOLDENROD);
+            }
+            for (uint8_t i = 0; i < 5; i++) {
+                rgb_matrix_set_color(top_right_leds[i], RGB_GOLDENROD);
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            for (uint8_t i = 0; i < 20; i++) {
+                rgb_matrix_set_color(sym_leds[i], RGB_ORANGE);
+            }
+            for (uint8_t i = 0; i < 5; i++) {
+                rgb_matrix_set_color(top_right_leds[i], RGB_ORANGE);
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            rgb_matrix_set_color(13, RGB_GOLDENROD);        // To Numbers
+            break;
+        case _FUNCTION:
+            for (uint8_t i = 0; i < 12; i++) {
+                rgb_matrix_set_color(fun_leds[i], RGB_MAGENTA);
+            }
+            for (uint8_t i = 0; i < 5; i++) {
+                rgb_matrix_set_color(top_right_leds[i], RGB_MAGENTA);
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            rgb_matrix_set_color(41, RGB_SPRINGGREEN);      // To gaming
+            rgb_matrix_set_color(40, RGB_PINK);             // To navigation
+            rgb_matrix_set_color(6, RGB_RED);               // To Adjust
+            break;
+        case _ADJUST:
+            rgb_matrix_set_color(23, RGB_GREEN);            // Reset key
+            for (uint8_t i = 0; i < 8; i++) {
+                rgb_matrix_set_color(adjust_leds[i], RGB_RED);
+            }
+            for (uint8_t i = 0; i < 9; i++) {
+                rgb_matrix_set_color(adjust_other_leds[i], RGB_RED);
+            }
+            rgb_matrix_set_color(6, RGB_CORAL);            // To qwerty
+            rgb_matrix_set_color(41, RGB_SPRINGGREEN);      // To gaming
+            rgb_matrix_set_color(40, RGB_PINK);             // To navigation
+            rgb_matrix_set_color(35, RGB_GOLDENROD);        // To number
+            break;
+    }
+}
+
+// CRKBD LED TEMPLATE (learned through much thought (and trial and error))
+//  ,-----------------------------------------------------.                    ,-----------------------------------------------------.
+//  |   24   |   23   |   18   |   17   |   10   |    9   |                    |   36   |   37   |   44   |   45   |   50   |   51   |
+//  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//  |   25   |   22   |   19   |   16   |   11   |    8   |                    |   35   |   38   |   43   |   46   |   49   |   52   |
+//  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//  |   26   |   21   |   20   |   15   |   12   |    7   |                    |   34   |   39   |   42   |   47   |   48   |   53   |
+//  `--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------'
+//                                      |   14   |   13   |   6    |  |   33   |   40   |   41   |
+//                                      `--------------------------'  `--------------------------'
+
+/*
+Colors (Not using HSV for some reason? idk)
+
+RGB	                HSV
+RGB_AZURE	        HSV_AZURE
+RGB_BLACK/RGB_OFF	HSV_BLACK/HSV_OFF
+RGB_BLUE	        HSV_BLUE
+RGB_CHARTREUSE	    HSV_CHARTREUSE
+RGB_CORAL	        HSV_CORAL
+RGB_CYAN	        HSV_CYAN
+RGB_GOLD	        HSV_GOLD
+RGB_GOLDENROD	    HSV_GOLDENROD
+RGB_GREEN	        HSV_GREEN
+RGB_MAGENTA	        HSV_MAGENTA
+RGB_ORANGE	        HSV_ORANGE
+RGB_PINK	        HSV_PINK
+RGB_PURPLE	        HSV_PURPLE
+RGB_RED	            HSV_RED
+RGB_SPRINGGREEN	    HSV_SPRINGGREEN
+RGB_TEAL	        HSV_TEAL
+RGB_TURQUOISE	    HSV_TURQUOISE
+RGB_WHITE	        HSV_WHITE
+RGB_YELLOW	        HSV_YELLOW
+
+*/
