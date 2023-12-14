af53980e6b9d121ade20a6395a507e7b86ff26c6
Elliott Claus <e.pneve.seven@gmail.com>
2023-12-14 08:20:47 -0500

    minus key

--- keyboards/crkbd/keymaps/emdashiiHJKL/keymap.c
+++ keyboards/crkbd/keymaps/emdashiiHJKL/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                             `--------------------------'  `--------------------------'
   [_NUMBER] = LAYOUT_split_3x6_3(
       XXXXXXX,    KC_GRV,  KC_F7,   KC_F8,   KC_F9,  KC_F10,                    S(KC_8),    KC_7,    KC_8,    KC_9, KC_PPLS, XXXXXXX,
-      XXXXXXX,   KC_QUOT,  KC_F4,   KC_F5,   KC_F6,  KC_F11,                     KC_EQL,    KC_4,    KC_5,    KC_6, KC_PPLS, XXXXXXX,
+      XXXXXXX,   KC_QUOT,  KC_F4,   KC_F5,   KC_F6,  KC_F11,                     KC_EQL,    KC_4,    KC_5,    KC_6, KC_MINS, XXXXXXX,
       XXXXXXX,S(KC_QUOT),  KC_F1,   KC_F2,   KC_F3,  KC_F12,                      COLON,    KC_1,    KC_2,    KC_3, KC_SLSH, XXXXXXX,
                                            KC_BSPC, LGAMING, LADJUST,       KC_ENT,   KC_0,  KC_DOT
 
