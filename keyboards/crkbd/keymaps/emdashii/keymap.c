406de67ad07152827351ca4072ec97426e54ce3e
Elliott Claus <e.pneve.seven@gmail.com>
2026-07-01 10:58:53 -0400

    Update crkbd keymap and QMK generator fixes

--- keyboards/crkbd/keymaps/emdashii/keymap.c
+++ keyboards/crkbd/keymaps/emdashii/keymap.c
@@ -49,13 +49,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 //  | MO Num |   Z    |   X    |   C    |   V    |   B    |                    |   N    |   M    |   ,    |   .    |   /    | Escape |
 //  `--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------'
-//                                      |   Del  | MO Nav | Enter  |  | OSL Fn |  Space | C-Bksp |
+//                                      |   Del  | MO Nav | Enter  |  | OSL Fn |Spc/Nav | C-Bksp |
 //                                      `--------------------------'  `--------------------------'
   [_QWERTY] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,  KC_BSPC,
       CW_TOGG,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,                         KC_H,  HOME_J,  HOME_K,  HOME_L,HOME_SCLN,  KC_QUOT,
    MO(_NUMBER),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH,   KC_ESC,
-                                  KC_DEL, MO(_NAVIGATION), KC_ENT,    LOSLFUN, KC_SPC, WORDDEL
+                                  KC_DEL, MO(_NAVIGATION), KC_ENT,    LOSLFUN, LT(_NAVIGATION, KC_SPC), WORDDEL
   ),
 
 // Navigation + Symbol
